105a6da215bcf409346ec914b7729e013460e932
versatile15 <cryptoexpert104@gmail.com>
2025-05-26 05:48:08 -0400

    fix: fix vtxSigOpsCost error

--- src/miner.cpp
+++ src/miner.cpp
@@ -220,6 +220,9 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
     pblock->nNonce         = 0;
     pblock->nNonce64         = 0;
     pblock->nHeight          = nHeight;
+    if (pblocktemplate->vTxSigOpsCost.empty()) {
+        pblocktemplate->vTxSigOpsCost.push_back(0);
+    }
     pblocktemplate->vTxSigOpsCost[0] = WITNESS_SCALE_FACTOR * GetLegacySigOpCount(*pblock->vtx[0]);
 
     CValidationState state;
