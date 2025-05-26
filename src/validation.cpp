0bc581ad80de8c237a1f54360101014b08222d69
versatile15 <cryptoexpert104@gmail.com>
2025-05-26 06:19:38 -0400

    fix: add powlimit

--- src/validation.cpp
+++ src/validation.cpp
@@ -4828,6 +4828,10 @@ static bool AcceptBlock(const std::shared_ptr<const CBlock>& pblock, CValidation
         return false;
 
     bool isPoS = block.IsProofOfStake();
+    // Enforce PoW cutoff at consensus.nLastPOWBlock
+    if (!isPoS && nHeight > consensus.kawpowHeight) {
+        return state.DoS(100, false, REJECT_INVALID, "pow-not-allowed-past-last-pow-block");
+    }
     if (!isPoS && block.GetHash() != consensus.hashGenesisBlock && !CheckWork(block, pindexPrev))
         return state.DoS(100, false, REJECT_INVALID);
 
