716d4c66e054ad3eaf13b5d0ccf4b57755368ebe
versatile15 <cryptoexpert104@gmail.com>
2025-05-27 04:11:23 -0400

    fix: add setstakemodifier

--- src/validation.cpp
+++ src/validation.cpp
@@ -2520,7 +2520,7 @@ static bool ConnectBlock(const CBlock& block, CValidationState& state, CBlockInd
         }
 
         if (fGeneratedStakeModifier) {
-            pindex->SetStakeModifierV1(nStakeModifier);
+            pindex->SetStakeModifier(nStakeModifier);
             pindex->nFlags |= BLOCK_STAKE_MODIFIER;
             LogPrintf("StakeModifier: Set at height %d: %016x\n", pindex->nHeight, nStakeModifier);
         }
