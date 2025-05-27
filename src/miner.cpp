46926d00560ceb6de05d4b4cdca980a0aadf8c81
versatile15 <cryptoexpert104@gmail.com>
2025-05-27 10:55:43 -0400

    fix: coinstake transaction fix

--- src/miner.cpp
+++ src/miner.cpp
@@ -308,6 +308,7 @@ bool SolveProofOfStake(CBlock* pblock, CBlockIndex* pindexPrev, CWallet* pwallet
     txCoinbase.vout.emplace_back(GetBlockValue(nHeight), scriptPubKeyIn);
     txCoinbase.vout[0].nValue = GetBlockValue(nHeight);
     txCoinbase.vout[0].scriptPubKey = CScript() << nHeight << OP_RETURN;
+    txCoinbase.nLockTime = nTxNewTime;
 
     // Sign coinstake
     if (!pwallet->SignCoinStake(txCoinStake)) {
