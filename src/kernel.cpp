5419b29585f6806e310cc05476b287e2d41a198d
versatile15 <cryptoexpert104@gmail.com>
2025-05-28 03:17:08 -0400

    fix: stake timestamp fix

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -115,12 +115,7 @@ bool Stake(const CBlockIndex* pindexPrev, CStakeInput* stakeInput, unsigned int 
 
     const bool fRegTest = GetParams().IsRegTestNet();
     nTimeTx = (fRegTest ? GetAdjustedTime() : GetCurrentTimeSlot());
-    if (nTimeTx <= pindexPrev->nTime && !fRegTest) {
-        nTimeTx = GetTimeSlot(pindexPrev->nTime + GetParams().GetConsensus().nTimeSlotLength);
-        LogPrintf("%s : nTimeTx = %d %d\n", __func__, nTimeTx, pindexPrev->nTime);
-        if (nTimeTx <= pindexPrev->nTime) return false;
-    }
-
+    if (nTimeTx <= pindexPrev->nTime && !fRegTest) return false;
     // Verify Proof Of Stake
     CStakeKernel stakeKernel(pindexPrev, stakeInput, nBits, nTimeTx);
     return stakeKernel.CheckKernelHash(true);
