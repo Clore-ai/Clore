9e3d898ec9f4f377745f9ac3f70d713635266ff7
versatile15 <cryptoexpert104@gmail.com>
2025-05-28 11:57:39 -0400

    fix: add declare set

--- src/base58.h
+++ src/base58.h
@@ -108,6 +108,7 @@ class CCloreAddress : public CBase58Data {
 public:
     bool Set(const CKeyID &id);
     bool Set(const CScriptID &id);
+    bool Set(const CExchangeKeyID &id);
     bool Set(const CTxDestination &dest);
     bool IsValid() const;
     bool IsValid(const CChainParams &params) const;
