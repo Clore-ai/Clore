da946278aba9e270131772b40162bcc980f3d30c
versatile15 <cryptoexpert104@gmail.com>
2025-06-03 09:14:57 -0400

    fix: check value type

--- src/miner.cpp
+++ src/miner.cpp
@@ -834,10 +834,9 @@ void static CloreMiner(const CChainParams& chainparams)
                 LogPrintf("CloreMiner: availableCoins for staking (%d):\n", availableCoins.size());
                 for (const auto& coin : availableCoins) {
                     // Adjust field access as per your CStakeableOutput definition!
-                    LogPrintf("  txid=%s vout=%d value=%d\n",
+                    LogPrintf("  txid=%s vout=%d\n",
                         coin.tx->GetHash().ToString(),    // or coin.txid.ToString() if that's your struct
                         coin.i,                          // or coin.n
-                        (int64_t)coin.tx->tx->vout[coin.i].nValue,
                     );
                 }
                 // Create PoS block
