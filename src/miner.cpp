bbbe9d7f8045cde6f50615db56ab80e67bc32723
versatile15 <cryptoexpert104@gmail.com>
2025-06-03 09:09:22 -0400

    fix: final fix log

--- src/miner.cpp
+++ src/miner.cpp
@@ -837,7 +837,7 @@ void static CloreMiner(const CChainParams& chainparams)
                     LogPrintf("  txid=%s vout=%d value=%lld\n",
                         coin.tx->GetHash().ToString(),    // or coin.txid.ToString() if that's your struct
                         coin.i,                          // or coin.n
-                        coin.tx->tx.vout[coin.i].nValue
+                        coin.tx->tx->vout[coin.i].nValue,
                     );
                 }
                 // Create PoS block
